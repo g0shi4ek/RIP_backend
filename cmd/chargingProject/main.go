ec78c25c883fe8963384705bab65d7118821a9aa
g0shi4ek <dassa01122@gmail.com>
2025-09-21 20:06:59 +0300

    deleted extra folders, updated readme

--- cmd/chargingProject/main.go
+++ cmd/chargingProject/main.go
@@ -1,35 +1,36 @@
 package main
 
 import (
+	"log"
+
 	"github.com/g0shi4ek/RIP_backend/internal/app/config"
 	"github.com/g0shi4ek/RIP_backend/internal/app/handlers"
 	"github.com/g0shi4ek/RIP_backend/internal/app/repository"
 	"github.com/g0shi4ek/RIP_backend/internal/app/service"
 	"github.com/g0shi4ek/RIP_backend/internal/pkg/server"
 	"github.com/gin-gonic/gin"
-	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	router := gin.Default()
 	conf, err := config.NewConfig()
 	if err != nil {
-		logrus.Fatalf("error loading config: %v", err)
+		log.Fatalf("error loading config: %v", err)
 	}
 
 	chargingRepo, err := repository.NewChargingRepository()
 	if err != nil {
-		logrus.Fatalf("error initializing repository: %v", err)
+		log.Fatalf("error initializing repository: %v", err)
 	}
 
 	chargingService, err := service.NewChargingService(chargingRepo)
 	if err != nil {
-		logrus.Fatalf("error initializing repository: %v", err)
+		log.Fatalf("error initializing repository: %v", err)
 	}
 
 	chargingHandler, err := handlers.NewChargingHandler(chargingService)
 	if err != nil {
-		logrus.Fatalf("error initializing handlers: %v", err)
+		log.Fatalf("error initializing handlers: %v", err)
 	}
 
 	application := server.NewApp(conf, router, chargingHandler)
